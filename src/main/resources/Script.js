5b8c64dd24110697628728eb09a3bfdad6d16f14
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2022-02-13 01:56:34 -0500

    Corrección de errores en la API

--- src/main/resources/Script.js
+++ src/main/resources/Script.js
@@ -1,11 +1,23 @@
 var buttonCtoF = document.getElementById("CalcularTempCaF");
 
-buttonCtoF.onclick = function(){
-    var TempC = document.getElementById("TempC");
-    axios.get('/calcFC/C/' + TempC).then(response => {
+buttonCtoF.onclick = function() {
+    console.log("Click Botón Calcular °F");
+    var TempC = document.getElementById("GET-TempC");
+    axios.get('/calcFC/C/' + TempC.value).then(response => {
             var rta = JSON.parse(response.data);
             var rtaFLabel = document.getElementById("rtaTempF");
-            $("#rtaTempF").text("<h2>" + TempC + " °Celsius = " + rta + " °Fahrenheit </h2>");
-            //rtaFLabel.innerHTML = "<h2>" + TempC + " °Celsius = " + rta + " °Fahrenheit </h2>";
+            rtaFLabel.innerHTML = TempC.value + " °Celsius = " + rta + " °Fahrenheit";
+        });
+}
+
+var buttonFtoC = document.getElementById("CalcularTempFaC");
+
+buttonFtoC.onclick = function() {
+    console.log("Click Botón Calcular °C");
+    var TempF = document.getElementById("GET-TempF");
+    axios.get('/calcFC/F/' + TempF.value).then(response => {
+            var rta = JSON.parse(response.data);
+            var rtaCLabel = document.getElementById("rtaTempC");
+            rtaCLabel.innerHTML = TempF.value + " °Fahrenheit = " + rta + " °Celsius";
         });
 }
